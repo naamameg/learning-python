7911b7d9ac7daababf8d82e8ebb38dc80c68a785
Roy <royee.livne6@gmail.com>
2021-09-04 16:19:57 +0300

    4.9.21

--- campus IL/season2/unit 2 Object Oriented Programming.py
+++ campus IL/season2/unit 2 Object Oriented Programming.py
@@ -26,7 +26,7 @@ def main():
 main()"""
 
 
-#2.3.4
+"""#2.3.4
 class Pixel:
     def __init__(self, coordinate_1, coordinate_2, color_1=0, color_2=0, color_3=0):
         self._x = coordinate_1
@@ -58,4 +58,117 @@ def main():
     pixel_1.set_grayscale()
     pixel_1.print_pixel_info()
 
+main()"""
+
+
+
+#2.2.2
+
+"""class Sparrow:
+    count_animals = 0
+    def __init__(self, age, name="octavio"):
+        self._name = name
+        self._age = age
+        Sparrow.count_animals += 1
+    def birthday(self):
+        self._age += 1
+    def get_age(self):
+        return self._age
+    def set_name(self, name):
+        self._name = name
+    def get_name(self):
+        return self._name
+
+def main():
+    sparrow_1 = Sparrow(4, "sparrow")
+    sparrow_2 = Sparrow(6)
+    print(sparrow_1.get_name())
+    print(sparrow_2.get_name())
+    sparrow_2.set_name("frogy")
+    print(sparrow_2.get_name())
+    print(Sparrow.count_animals)
+    print(dir(Sparrow))
 main()
+"""
+
+
+
+"""def main():
+    sparrow_1 = Sparrow(2, "sprey")
+    sparrow_2 = Sparrow(4, "super")
+    sparrow_1.birthday()
+    print(sparrow_1.get_age())
+    print(sparrow_2.get_age())
+main()"""
+
+"""#2.4.2
+class BigThing:
+    def __init__(self, word):
+        self._word = word
+    def size(self):
+        if isinstance(self._word, int):
+            return self._word
+        elif isinstance(self._word, dict) or isinstance(self._word, list) or isinstance(self._word, str):
+            return len(self._word)
+
+class BigCat(BigThing):
+    def __init__(self, word, weight):
+        BigThing.__init__(self, word)
+        self._weight = weight
+    def size(self):
+        super().size()
+        if self._weight > 20:
+            print("very fat")
+        elif self._weight > 15:
+            print("fat")
+
+        else:
+            print("OK")
+
+def main():
+    animal_1 = BigCat("pif", 76)
+    animal_1.size()
+    animal_2 = BigThing("lush")
+    print(animal_2.size())
+main()"""
+
+
+#תרגיל מסכם
+#2.5
+
+class Animal:
+    def __init__(self, name, hunger=0):
+        self._mame = name
+        self._hunger = hunger
+    def get_name(self):
+        return self._mame
+    def 
+
+
+class Dog(Animal):
+    pass
+
+class Cat(Animal):
+    pass
+
+class Skunk(Animal):
+    pass
+
+class Unicorn(Animal):
+    pass
+
+class Dragon(Animal):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
